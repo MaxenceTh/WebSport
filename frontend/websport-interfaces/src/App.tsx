c4cc32bf0948881a4f25a139e18352f4a43b1249
MaxenceTh <thomas.maxence141v2@gmail.com>
2025-09-23 15:06:52 +0200

    first push

---
+++ frontend/websport-interfaces/src/App.tsx
@@ -0,0 +1,17 @@
+import {  } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-center'>Coucou c'est moi</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
